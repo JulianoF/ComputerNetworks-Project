3df268dcc7ab3872840fa87ed051d05f5dc70cb3
JulianoF <juliano1@live.ca>
2024-03-14 09:08:13 -0400

    Server/Client code (needs testing)

--- HelloWorldTCP/helloClient.c
+++ HelloWorldTCP/helloClient.c
@@ -1,3 +1,68 @@
-#include <stdio.h>
+#define SERVER_TCP_PORT 8000
+#define BUFLEN 256
+#define IP "127.0.0.1" 
 
-int main(void){}
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/signal.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+int main(int argc, char** argv){
+    int sd, port;
+    struct sockaddr_in server;
+    char buffer[BUFLEN];
+    
+    if(argc == 2){
+        port = atoi(argv[1]);
+        if(port == 0){
+            port = SERVER_TCP_PORT;
+        }
+    }else{
+        port = SERVER_TCP_PORT;
+    }
+
+    if((sd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }else{
+        printf("Socket successfully created\n");
+    }
+
+    memset(&server, 0, sizeof(server));
+    server.sin_family = AF_INET;
+    server.sin_port = htons(port);
+    server.sin_addr.s_addr = inet_addr(IP);
+
+    if(bind(sd, (struct sockaddr *)&server, sizeof(server)) < 0){
+        perror("Can't bind name to socket\n");
+        exit(EXIT_FAILURE);
+    }else{
+        printf("Socket successfully binded\n");
+    }
+       // connect the client socket to server socket
+    if (connect(sd, (struct sockaddr *)&server, sizeof(server)) != 0) {
+        perror("connection with the server failed\n");
+        exit(EXIT_FAILURE);
+    }else{
+        printf("Connected to the server\n");
+    }
+
+    printf("Waiting for Data..\n");
+    while(1){
+        ssize_t status = read(sd, buffer, sizeof(buffer));
+        if(status == 0){
+            break;
+        }else{
+            printf("Message from Server: %s", buffer);
+        }
+
+    }
+    close(sd);
+    return 0;
+}
--- HelloWorldTCP/helloServer.c
+++ HelloWorldTCP/helloServer.c
@@ -1,3 +1,76 @@
-#include <stdio.h>
+#define SERVER_TCP_PORT 8000
+#define BUFLEN 256 
+#define MESSAGE "Hello"
 
-int main(void){}
+#include <stdio.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/signal.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char** argv){
+    int sd, new_sd, client_len, port;
+    struct sockaddr_in server, client;
+    char buffer[BUFLEN];
+    
+    if(argc == 2){
+        port = atoi(argv[1]);
+        if(port == 0){
+            port = SERVER_TCP_PORT;
+        }
+    }else{
+        port = SERVER_TCP_PORT;
+    }
+    printf("Opening TCP Server on Port %d\n",port);
+      
+    if((sd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }else{
+        printf("Socket successfully created\n");
+    }
+
+    memset(&server, 0, sizeof(server));
+    server.sin_family = AF_INET;
+    server.sin_port = htons(port);
+    server.sin_addr.s_addr = htonl(INADDR_ANY);
+    
+
+    if(bind(sd, (struct sockaddr *)&server, sizeof(server)) < 0){
+        perror("Can't bind name to socket\n");
+        exit(EXIT_FAILURE);
+    }else{
+        printf("Socket successfully binded\n");
+    }
+
+    if((listen(sd, 5)) != 0) { 
+        perror("Listen failed\n"); 
+        exit(EXIT_FAILURE); 
+    }else{
+        printf("Server listening...\n");
+    }
+
+    while(1) {
+        ssize_t bytes_written = 0;
+        client_len = sizeof(client);
+        new_sd = accept(sd, (struct sockaddr *)&client, &client_len);
+
+        if(new_sd < 0){
+            perror("Can't accept client \n");
+            exit(EXIT_FAILURE);
+        }
+        bytes_written = (new_sd, MESSAGE, strlen(MESSAGE),0);
+        if(bytes_written >= 20){
+            close(new_sd);
+            break;
+        }
+    }
+
+    close(sd);
+
+}
